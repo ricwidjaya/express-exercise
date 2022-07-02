5ac748650290354d889a49aede75ff5055e7d560
Richard Widjaya <ricwidjaya@gmail.com>
2022-07-02 14:10:30 +0800

    project init

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res, next) => {
+  return res.send(`<h1>Hello, World.</h1>`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Express App is running on ${PORT}`)
+})
