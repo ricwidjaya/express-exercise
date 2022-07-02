1f7511cb2f0d80056e4cf850aa9fb089cd9e89a6
Richard Widjaya <ricwidjaya@gmail.com>
2022-07-02 16:15:13 +0800

    fix: commit

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const router = require('./routes')
+const handlebarsHelper = require('./helpers/handlebars-helper')
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res, next) => {
-  return res.send(`<h1>Hello, World.</h1>`)
-})
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs.engine({ extname: '.hbs', helpers: handlebarsHelper }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(router)
 
 app.listen(PORT, () => {
   console.log(`Express App is running on ${PORT}`)
